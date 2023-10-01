1dd001e10eb97048bd2dfee433a604fe008f669d
kmkinney <kevinmk712@gmail.com>
2023-10-01 15:46:24 -0600

    Move test html

--- tests/server/parseProblem.test.ts
+++ tests/server/parseProblem.test.ts
@@ -1,14 +1,9 @@
 import { test } from "vitest";
 import fs from "fs";
-import {
-  fetchProblemHtml,
-  parseProblem,
-} from "~/server/api/services/problemParser";
+import { parseProblem } from "~/server/api/services/problemParser";
 
 test("parse a problem from the url", async () => {
-  // const URL = "https://open.kattis.com/problems/1dfroggereasy?tab=metadata";
-  // await fetchProblemHtml(URL);
-  const html = fs.readFileSync("./test.html", "utf-8");
-  const result = await parseProblem(html);
+  const html = fs.readFileSync("./tests/test.html", "utf-8");
+  const result = parseProblem(html);
   console.log(result);
 });
